e2f7992a63b0e9f59d0b22a63297ebbb77e76012
SEGHIRSamy <sam09012003@gmail.com>
2024-09-17 17:24:37 +0200

    Fin TP2

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -0,0 +1,8 @@
+package fr.univavignon.pokedex.api;
+
+import junit.framework.TestCase;
+
+public class IPokemonFactoryTest extends TestCase {
+    //Il faut faire les tests unitaires pour cette classe
+    //Surement genre ne pas pouvoir mettre des PV négatifs ou quoi
+}
