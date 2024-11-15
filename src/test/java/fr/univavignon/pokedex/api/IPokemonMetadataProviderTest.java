7e05c343522a4e2ddd353d94ea4c62e20cd53d84
SEGHIRSamy <sam09012003@gmail.com>
2024-11-15 21:48:13 +0100

    TP4 - implementation

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -24,4 +24,25 @@ public class IPokemonMetadataProviderTest {
         assertEquals(126, metadata.getDefense());
         assertEquals(90, metadata.getStamina());
     }
+
+    @Test
+    public void testGetPokemonMetadataForAquali() throws PokedexException {
+        PokemonMetadata metadata = metadataProvider.getPokemonMetadata(133);
+        assertNotNull(metadata);
+        assertEquals(133, metadata.getIndex());
+        assertEquals("Aquali", metadata.getName());
+        assertEquals(186, metadata.getAttack());
+        assertEquals(168, metadata.getDefense());
+        assertEquals(260, metadata.getStamina());
+    }
+
+    @Test(expected = PokedexException.class)
+    public void testGetPokemonMetadataWithInvalidIndex() throws PokedexException {
+        metadataProvider.getPokemonMetadata(-1);
+    }
+
+    @Test(expected = PokedexException.class)
+    public void testGetPokemonMetadataWithIndexOutOfBounds() throws PokedexException {
+        metadataProvider.getPokemonMetadata(155);
+    }
 }
